547cbb827783ef40e64a55d71eeece5b5c165c07
Sergio Vilar <sergiovilar.r@gmail.com>
2017-11-20 20:37:56 -0300

    Atualiza Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.0"
+lock "3.10.0"
 
 server '138.197.41.12', user: 'root', port: 22, roles: [:web, :app, :db], primary: true
 set :application, "odindb"
